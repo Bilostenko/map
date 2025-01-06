fd9d4daaa8be80e650ecb9bd6b9229ad93b903e1
BILOSTENKO_D <bilostenkod@gmail.com>
2025-01-06 17:58:09 +0200

    add User and Company class

---
+++ dist/index.377278e2.js
@@ -0,0 +1,6 @@
+// import { User } from "./User";
+// import { CustomMap } from "./CustomMap"
+// import { CustomMap } from "./CustomMap";
+// new CustomMap('map')
+
+//# sourceMappingURL=index.377278e2.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import { User } from "./User";
 import { Company } from "./Company";
 
-new google.maps.Map(document.getElementById('map'))
+import { CustomMap } from "./CustomMap";
+
+const user = new User();
+const company = new Company();
+
+const customMap = new CustomMap('map')
+
+
+customMap.addMarker(user)
+customMap.addMarker(company)
